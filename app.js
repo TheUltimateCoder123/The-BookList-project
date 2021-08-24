fdca42fd34e9dbb87d692cf2fb1e909799ab6033
Hemant Devkota <hemantdevkota100billion@gmail.com>
2021-08-24 19:16:16 +0530

    finally application is ready

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ this.isbn=isbn;
 }
 //////UI constructer
 function UI(){}
+
 //addBookdetails Prototype Inside UI
 UI.prototype.addBookDetails=function(book){
 
@@ -31,6 +32,32 @@ UI.prototype.clear=function(){
 
 
 }
+//ShowWarning Protypes 
+UI.prototype.showWarning=function(warningText,className){
+const form=document.querySelector('#formid');
+const parent=document.querySelector('.wrapper');
+const newDiv=document.createElement('div');
+newDiv.className=` alert ${className}`;
+newDiv.appendChild((document.createTextNode(warningText)));
+parent.insertBefore(newDiv,form);
+setTimeout(function(){
+document.querySelector('.alert').remove();
+},2000)
+
+
+
+}
+//delete Prototype
+UI.prototype.delete=function(targetElement){
+if(targetElement.className==="fas fa-times"){
+  targetElement.parentElement.parentElement.remove();
+
+
+
+}
+
+ 
+}
 
  /////////////////////////////////////////////////EVent Listerners to form
 document.querySelector('#formid').addEventListener('submit',function(e){
@@ -42,10 +69,21 @@ const btitle=document.getElementById('Bname').value,
     const bookdetails=new Book(btitle,bauthor,bisbn);
 //This is UI object
     const ui=new UI();
+    if(btitle=="" || bauthor=="" || bisbn==""){
+      ui.showWarning('Please Enter The Value','warning');
+    }
+    else{
+      //Calling prototypes like function
+      ui.showWarning('Book added Succesfully','success');
+      ui.addBookDetails(bookdetails);
+      ui.clear();
+    }
   
-    //Calling prototypes like function
-    ui.addBookDetails(bookdetails);
-    ui.clear();
+  
+
+
+
+  
    
    
 
@@ -55,4 +93,12 @@ const btitle=document.getElementById('Bname').value,
 
 
     e.preventDefault();
+})
+document.querySelector('table').addEventListener('click',function(e){
+
+  const ui=new UI();
+ui.delete(e.target);
+ui.showWarning('Book Removed!!!','success');
+
+  e.preventDefault();
 })
